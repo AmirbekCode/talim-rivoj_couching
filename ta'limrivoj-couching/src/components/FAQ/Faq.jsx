590a70542df88f815f3136282575ef958fc2013d
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-05-29 23:25:34 +0500

    Push faq.css and Faq.jsx

---
+++ ta'limrivoj-couching/src/components/FAQ/Faq.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Faq() {
+  return (
+    <div>
+      <div className="container">
+        <h1>"Biznes Kouching: Tizimlashtirish va xodimlarni boshqarish" dasturi  </h1>
+      </div>
+    </div>
+  )
+}
