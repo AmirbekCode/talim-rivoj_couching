4f5266664d74a68e57c8bb92f26241dd3aae9bdd
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-05-29 23:28:44 +0500

    Udate Faq.jsx

--- ta'limrivoj-couching/src/components/FAQ/Faq.jsx
+++ ta'limrivoj-couching/src/components/FAQ/Faq.jsx
@@ -5,6 +5,11 @@ export default function Faq() {
     <div>
       <div className="container">
         <h1>"Biznes Kouching: Tizimlashtirish va xodimlarni boshqarish" dasturi  </h1>
+        <div className="accordion">
+            <div className="accordion-item">
+                <button id="accordion-button-1" aria-expanded="false"><span className="accordion-title">Biznes model</span></button>
+            </div>
+        </div>
       </div>
     </div>
   )
