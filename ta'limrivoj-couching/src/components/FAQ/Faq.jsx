5fd164b6c47d31a851ff9184840cce4ac4b5314d
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-05-29 23:33:59 +0500

    add ul li

--- ta'limrivoj-couching/src/components/FAQ/Faq.jsx
+++ ta'limrivoj-couching/src/components/FAQ/Faq.jsx
@@ -7,7 +7,18 @@ export default function Faq() {
         <h1>"Biznes Kouching: Tizimlashtirish va xodimlarni boshqarish" dasturi  </h1>
         <div className="accordion">
             <div className="accordion-item">
-                <button id="accordion-button-1" aria-expanded="false"><span className="accordion-title">Biznes model</span></button>
+                <button id="accordion-button-1" aria-expanded="false"><span className="accordion-title">Biznes model</span><span className='icon' aria-hidden="true"></span></button>
+                <div className="accordion-content">
+                  <ul>
+                    <li>Biznes modelning tuzilishi</li>
+                    <li>Biznes modelni tuzish qadamlari</li>
+                    <li>Biznes modelni tuzish va qo‘llash mijoz kim ular bilan qanday munosabat ornatadi asosiy xarajatlari qaysilar va ularni qanday tashkillashtiradi</li>
+                    <li>Faoliyati davomida kimlar bilan hamkorlik qiladi</li>
+                    <li>Bozorga qanday taklif beroladi qanday xizmat ko‘rsatiladi</li>
+                    <li>Qaysi sotuv kanallaridan foydalaniladi</li>
+                    <li>Pul oqimlari va daromadlar qayerdan tashkillashtiriladi</li>
+                  </ul>
+                </div>
             </div>
         </div>
       </div>
