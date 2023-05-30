73a63012ac1c254b05e12b52273ba0fd9c3fdb85
AmirbekCode <106994015+AmirbekCode@users.noreply.github.com>
2023-05-30 13:36:47 +0500

    yeah

--- ta'limrivoj-couching/src/App.jsx
+++ ta'limrivoj-couching/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import Accordion from './components/FAQ/Accordions'
 import Projectinfo from './components/ProjectInfo/ProjectInfo'
 import Navbar from "./components/Navbar/Navbar"
 import { ChakraProvider } from '@chakra-ui/react'
@@ -9,7 +8,6 @@ function App() {
   return (
     <ChakraProvider>
       <Navbar />
-      <Accordion />
       <Projectinfo />
     </ChakraProvider>
 
