a2b5640360ab34f6ce4822d0c7ca76751fdaf3ea
AmirbekCode <106994015+AmirbekCode@users.noreply.github.com>
2023-05-30 06:40:58 +0500

    app jsx

--- ta'limrivoj-couching/src/App.jsx
+++ ta'limrivoj-couching/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Faq from './components/FAQ/Faq'
+import Projectinfo from './components/ProjectInfo/ProjectInfo'
+
 
 function App() {
 
   return (
     <div className="App">
       <Faq/>
+      <Projectinfo/>
     </div>
   )
 }
