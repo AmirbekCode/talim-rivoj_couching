59ce5128bfa390bd890f5d0e562f8996edea1ed6
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-05-31 15:36:47 +0500

    Update Faq.jsx

--- ta'limrivoj-couching/src/components/FAQ/Faq.jsx
+++ ta'limrivoj-couching/src/components/FAQ/Faq.jsx
@@ -1,7 +1,43 @@
 import React, { useState } from 'react';
-import '../FAQ/faq.css'; // Import the CSS file for styling
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-const Faq = ({ faqData }) => {
+import '../FAQ/faq.css';
+
+const Faq = () => {
+  const faqData = [
+    {
+      question: 'Biznbes model',
+      answers: [
+        {
+          id: 1,
+          content: 'Biznes modelning tuzilishi',
+        },
+        {
+          id: 2,
+          content: 'Biznes modelni tuzish qadamlari',
+        },
+        {
+          id: 3,
+          content: 'Faoliyati davomida kimlar bilan hamkorlik qiladi',
+        },
+        {
+          id: 4,
+          content: 'Bozorga qanday taklif beroladi qanday xizmat korsatiladi',
+        },
+        {
+          id: 5,
+          content: 'Qaysi sotuv kanallaridan foydalaniladi',
+        },
+      ],
+    },
+    
+    // Add more FAQ items as needed
+  ];
+
   const [activeIndex, setActiveIndex] = useState(null);
 
   const handleClick = (index) => {
@@ -10,19 +46,28 @@ const Faq = ({ faqData }) => {
 
   const renderQuestions = () => {
     return faqData.map((item, index) => (
-      <div key={index} className="faq-item">
-        <div
-          className={`faq-question ${activeIndex === index ? 'active' : ''}`}
-          onClick={() => handleClick(index)}
-        >
-          {item.question}
-          <span className="faq-icon">
-            {activeIndex === index ? '-' : '+'}
-          </span>
+      <div className="content">
+        <div className="header-title">
+          <h1>"Biznes Kouching: Tizimlashtirish va xodimlarni boshqarish" dasturi</h1>
         </div>
-        {activeIndex === index && (
-          <div className="faq-answer">{item.answer}</div>
-        )}
+        <div key={index} className='faq-item'>
+          <Accordion className='main-content' expanded={activeIndex === index} onChange={() => handleClick(index)}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls={`panel-${index}-content`}
+              id={`panel-${index}-header`}
+            >
+              <Typography className={`faq-question ${activeIndex === index ? 'active' : ''}`}>
+                {item.question}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              {item.answers.map((answer) => (
+                <Typography className='faq-answer' key={answer.id}>{answer.content}</Typography>
+              ))}
+            </AccordionDetails>
+          </Accordion>
+        </div>
       </div>
     ));
   };
