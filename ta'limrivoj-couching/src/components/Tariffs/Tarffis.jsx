183a4023664fa6136e4574379961e5177632711a
Muhammad <120216920+komilovmDev@users.noreply.github.com>
2023-06-07 03:34:57 +0500

    tarfiss img add

--- ta'limrivoj-couching/src/components/Tariffs/Tarffis.jsx
+++ ta'limrivoj-couching/src/components/Tariffs/Tarffis.jsx
@@ -1,15 +1,15 @@
-
+import './tarffis.css'
 
 
 export default function Tarffis() {
     return (
-        <div className="tarffis">   
+        <div className="tarffis">
             <span className="tarffisTilte">Tariflar</span>
             <div className="tarffisCons">
-                <div className="tarffisConsBox">
-                    <span></span>
-                </div>
+                <img src="https://static.tildacdn.com/tild3937-3230-4332-b738-306361376664/Frame_68.svg" alt="" />
+                <img src="https://static.tildacdn.com/tild3065-3236-4263-a432-363835313230/Frame_67.svg" alt="" />
+                <img src="https://static.tildacdn.com/tild3531-3466-4530-a437-376266303232/Frame_69.svg" alt="" />
             </div>
-        </div>      
+        </div>
     )
 }
